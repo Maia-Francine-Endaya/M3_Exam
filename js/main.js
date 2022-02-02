398b3d6ec8dfd8dcc6f9b936a5ca458b7ebf08b2
Maia Francine Endaya <maia.endaya@gmail.com>
2022-02-02 10:15:23 +0800

    added_rocks

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ function lighting() {
   scene.add(directionalLight);
 
   const pointLight = new THREE.PointLight(0xf5b158, 3.5, 100);
-  pointLight.position.set(0, 2, -13);
+  pointLight.position.set(0, 2, -12);
   scene.add(pointLight);
 }
 
@@ -32,6 +32,7 @@ camera.position.z = 35;
 //initializing textures
 const seaTexture = new THREE.TextureLoader().load('assets/textures/water_texture.jpg');
 const sandTexture = new THREE.TextureLoader().load('assets/textures/beach_sand_texture.jpg');
+const rockTexture = new THREE.TextureLoader().load('assets/textures/rocky_texture.jpg');
 
 //Shore
 const shoreGeo = new THREE.PlaneGeometry(320, 140);
@@ -49,6 +50,67 @@ const seaMesh = new THREE.Mesh(seaGeo, seaMat);
 
 scene.add(seaMesh);
 
+//Rock Geometries and Material
+const rockMat = new THREE.MeshLambertMaterial({ map: rockTexture });
+
+const rockGeo1 = new THREE.CylinderGeometry(2, 5, 11, 15, 6);
+
+const verticesOfCube = [
+  -1, -1, -1, 1, -1, -1, 1, 1, -1, -1, 1, -1,
+  -1, -1, 1, 1, -1, 1, 1, 1, 1, -1, 1, 1,
+];
+
+const indicesOfFaces = [
+  2, 1, 0, 0, 3, 2,
+  0, 4, 7, 7, 3, 0,
+  0, 1, 5, 5, 4, 0,
+  1, 2, 6, 6, 5, 1,
+  2, 3, 7, 7, 6, 2,
+  4, 5, 6, 6, 7, 4
+];
+
+const rockGeo2 = new THREE.PolyhedronGeometry(verticesOfCube, indicesOfFaces, 6, 2);
+
+const length = 12, width = 8;
+
+const shape = new THREE.Shape();
+shape.moveTo(0, 0);
+shape.lineTo(0, width);
+shape.lineTo(length, width);
+shape.lineTo(length, 0);
+shape.lineTo(0, 0);
+
+const extrudeSettings = {
+  steps: 5,
+  depth: 16,
+  bevelEnabled: true,
+  bevelThickness: 3,
+  bevelSize: 2,
+  bevelOffset: 1,
+  bevelSegments: 3
+};
+
+const rockGeo3 = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+
+//So many rocks
+rockMesh1 = new THREE.Mesh(rockGeo1, rockMat);
+rockMesh2 = new THREE.Mesh(rockGeo1, rockMat);
+rockMesh3 = new THREE.Mesh(rockGeo1, rockMat);
+
+rockMesh4 = new THREE.Mesh(rockGeo2, rockMat);
+rockMesh5 = new THREE.Mesh(rockGeo2, rockMat);
+rockMesh6 = new THREE.Mesh(rockGeo2, rockMat);
+
+
+//Really many sea rocks
+scene.add(rockMesh1);
+scene.add(rockMesh2);
+scene.add(rockMesh3);
+scene.add(rockMesh4);
+scene.add(rockMesh5);
+scene.add(rockMesh6);
+
+
 
 function animate() {
   requestAnimationFrame(animate);
@@ -57,17 +119,41 @@ function animate() {
   //Sea Position
   seaMesh.position.x = 5;
   seaMesh.position.y = -5;
-  seaMesh.position.z = -29;
+  seaMesh.position.z = -30;
 
   seaMesh.rotation.x = 1.6;
-  seaMesh.rotation.z = -0.32;
+  seaMesh.rotation.z = -0.29;
 
   //Shore Position
   shoreMesh.position.x = 22;
-  shoreMesh.position.y = -10;
+  shoreMesh.position.y = -6;
 
   shoreMesh.rotation.x = 1.6;
 
+  //Rocks
+  rockMesh1.position.x = -11.6;
+  rockMesh1.position.y = -1.7;
+  rockMesh1.position.z = 6.2;
+
+  rockMesh4.position.x = -45.3;
+  rockMesh4.position.y = -8;
+  rockMesh4.position.z = 1.7;
+
+  rockMesh5.position.x = -15.7;
+  rockMesh5.position.y = -6.7;
+  rockMesh5.position.z = 6.3;
+
+  rockMesh2.position.x = 3.2;
+  rockMesh2.position.y = -1.5;
+  rockMesh2.position.z = 14;
+
+  rockMesh6.position.x = 6.5;
+  rockMesh6.position.y = -8.9;
+  rockMesh6.position.z = 12.7;
+
+  rockMesh3.position.x = 22.4;
+  rockMesh3.position.y = -1;
+  rockMesh3.position.z = -2;
 
 
   renderer.render(scene, camera);
